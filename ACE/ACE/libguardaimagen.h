d1fad3f8d64c01eb5013aec95a5741277310ff9f
ismafc@gmail.com <ismafc@gmail.com@654a2211-555d-b977-7330-fc5d5f669db8>
2011-04-25 14:32:08 +0000

    libguardaimagen.h

---
+++ ACE/ACE/libguardaimagen.h
@@ -0,0 +1,19 @@
+#ifndef _LIBGUARDAIMAGEN_H_
+#define _LIBGUARDAIMAGEN_H_
+
+#include <stdio.h>
+
+/* 
+	Guarda en el archivo de nombre dado una imagen PGM de dimensiones anchura X altura 
+	que contiene en sus píxeles valores enteros entre pixel_min y pixel_max 
+*/
+void guardaPGMi (char* nombre, int anchura, int altura, int *pixels, int pixel_min, int pixel_max);
+void guardaPGMi (char* nombre, int anchura, int altura, int **pixels, int pixel_min, int pixel_max);
+
+/* 
+	Guarda en el archivo de nombre dado una imagen PGM de dimensiones anchura X altura 
+	que contiene en sus píxeles valores reales entre pixel_min y pixel_max
+*/
+void guardaPGMd (char* nombre, int anchura, int altura, double *pixels, double pixel_min, double pixel_max);
+
+#endif
