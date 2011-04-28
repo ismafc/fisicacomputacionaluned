d6692a62f3e1b60c3172a83116115d8d5a807e69
ismafc@gmail.com <ismafc@gmail.com@654a2211-555d-b977-7330-fc5d5f669db8>
2011-04-28 16:09:04 +0000

    Comentarios y mínimo avance en la evolución de la distancia de hamming

--- ACE/ACE/main.cpp
+++ ACE/ACE/main.cpp
@@ -101,6 +101,42 @@ void generarHamming(int** ACE, int regla, int pasos, int celdas)
 	}
 }
 
+/*
+ * Nombre: ACE (Autómata Celular Elemental)
+ * Autor: Ismael Flores Campoy
+ * Fecha: 28/04/2011
+ * Descripción: Genera información a propósito de la evolución de autómatas celulares elementales
+ * Sintaxis: ACE <opcion1>:<valor1> <opcion2>:<valor2> ...
+ *
+ * Opción					| Valores (separados por comas)		| Valor por defecto
+ * -------------------------------------------------------------------------------------------------------
+ * inicializacion			| aleatoria, semilla				| semilla
+ * hamming					| si								| no
+ * regla					| [0, 255], todas					| REGLA (54)
+ * pasos					| [1, 5000]							| PASOS (500)
+ * celdas					| [2, 10000]						| CELDAS (1000)
+ *
+ * Argumento					| Significado
+ * ------------------------------------------------------------------------------------------------------------
+ * inicializacion:aleatoria		| La primera fila del ACE contiene una sucesión aleatoria de '0' y '1'
+ * inicializacion:semilla		| La primera fila del ACE contiene todo '0' menos un '1' en la posición central
+ * hamming:si					| Se calcula la evolución de la distancia de hamming en el tiempo
+ *								| entre el ACE calculado y otro que difiere únicamente en el valor central 
+ *								| de la primera fila del ACE. Cada evolución calculada se guarda en un fichero.
+ * regla:todas					| Se calculan los ACEs (y se guardan en ficheros) de todas las reglas [0, 255]
+ * regla:4						| Se calcula el ACE (y se guarda en ficheros) de la regla 4
+ * pasos:300					| Se calculan 300 pasos de la evolución del ACE
+ * celdas:700					| El ACE lo conforman 700 posiciones
+ * 
+ *
+ * Ejemplos:
+ *
+ * ACE regla:126
+ * ACE hamming:si inicializacion:aleatoria
+ * ACE regla:todas
+ * ACE regla:4 hamming:si pasos:200 celdas:200
+ *
+ */
 int main(int argc, char** argv)
 {
 	int regla = REGLA;								// Regla a aplicar (por defecto REGLA)
@@ -111,7 +147,7 @@ int main(int argc, char** argv)
 	char strInicializacion[32];						// Guardamos el tipo de inicialización para generar el nombre del fichero
 	bool hamming = false;							// Guardamos si hay que calcular la evolución de la distancia de hamming
 
-	// Inicializamos el texto de la inicialización del ACE como "semilla"
+	// Inicializamos el texto de la inicialización del ACE como "semilla" (se usará para el nombre del fichero PGM en el que se guardan los resultados)
 	strcpy(strInicializacion, "semilla");
 
 	// Inicializamos la semilla de los números aleatorios
@@ -120,6 +156,7 @@ int main(int argc, char** argv)
 	// Procesado de los parámetros de entrada (si existen)
 	for (int a = 2; a <= argc; a++) {
 		if (strstr(argv[a -1], "inicializacion:") == argv[a - 1]) {
+			// Si encontramos un argumento 'inicialización:' analizamos que valor tiene.
 			if (strstr(argv[a -1], ":aleatoria") != NULL) {
 				strcpy(strInicializacion, "aleatoria");
 				inicializacion = INICIALIZACION_ALEATORIA;
@@ -130,10 +167,12 @@ int main(int argc, char** argv)
 			}
 		}
 		else if (strstr(argv[a -1], "hamming:") == argv[a - 1]) {
+			// Si encontramos un argumento 'hamming:' analizamos que valor tiene.
 			if (strstr(argv[a -1], ":si") != NULL)
 				hamming = true;
 		}
 		else if (strstr(argv[a -1], "regla:") == argv[a - 1]) {
+			// Si encontramos un argumento 'regla:' analizamos que valor tiene.
 			if (strstr(argv[a -1], ":todas") != NULL) 
 				regla = -1;
 			else {
@@ -145,6 +184,7 @@ int main(int argc, char** argv)
 			}
 		}
 		else if (strstr(argv[a -1], "celdas:") == argv[a - 1]) {
+			// Si encontramos un argumento 'celdas:' analizamos que valor tiene.
 			celdas = atoi(argv[a - 1] + strlen("celdas:"));
 			if (celdas <= MIN_CELDAS || celdas >= MAX_CELDAS || errno != 0) {
 				celdas = CELDAS;
@@ -152,6 +192,7 @@ int main(int argc, char** argv)
 			}
 		}
 		else if (strstr(argv[a -1], "pasos:") == argv[a - 1]) {
+			// Si encontramos un argumento 'pasos:' analizamos que valor tiene.
 			pasos = atoi(argv[a - 1] + strlen("pasos:"));
 			if (pasos <= MIN_PASOS || pasos >= MAX_PASOS || errno != 0) {
 				pasos = PASOS;
@@ -160,32 +201,17 @@ int main(int argc, char** argv)
 		}
 	}
 
-	if (regla == TODAS_LAS_REGLAS) {
-		for (int r = MIN_REGLA; r <= MAX_REGLA; r++) { 
-			/* definimos la condición inicial de nuestro ACE y asignamos la memoria necesaria dinámicamente */
-			inicializarACE(&ACE, pasos, celdas, inicializacion);
-
-			generarACE(ACE, r, pasos, celdas);
-	
-			char nombreFichero[256];
-			sprintf(nombreFichero, "ACE_R%03d_%s.pgm", r, strInicializacion);
-			guardaPGMi(nombreFichero, pasos + 1, celdas + 2, ACE, 1, 0);
-
-			if (hamming)
-				generarHamming(ACE, r, pasos, celdas);
-		}
-	}
-	else {
-		/* definimos la condición inicial de nuestro ACE y asignamos la memoria necesaria dinámicamente */
+	for (int r = ((regla == TODAS_LAS_REGLAS) ? MIN_REGLA : regla); r <= ((regla == TODAS_LAS_REGLAS) ? MAX_REGLA : regla); r++) { 
+		// definimos la condición inicial de nuestro ACE y asignamos la memoria necesaria dinámicamente
 		inicializarACE(&ACE, pasos, celdas, inicializacion);
 
-		generarACE(ACE, regla, pasos, celdas);
-	
+		generarACE(ACE, r, pasos, celdas);
+
 		char nombreFichero[256];
-		sprintf(nombreFichero, "ACE_R%03d_%s.pgm", regla, strInicializacion);
-		guardaPGMi(nombreFichero, pasos + 1, celdas + 2, ACE, 1, 0);
+		sprintf(nombreFichero, "ACE_R%03d_%s.pgm", r, strInicializacion);
+		guardaPGMiACE(nombreFichero, pasos + 1, celdas + 2, ACE, 1, 0);
 
 		if (hamming)
-			generarHamming(ACE, regla, pasos, celdas);
+			generarHamming(ACE, r, pasos, celdas);
 	}
 }
